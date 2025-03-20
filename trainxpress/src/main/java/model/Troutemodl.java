3648239c9b5421d3a39f382cdaafbcd05349149c
Savin Udana <saween910@gmail.com>
2025-03-20 16:37:43 +0530

    addModel Train route

---
+++ trainxpress/src/main/java/model/Troutemodl.java
@@ -0,0 +1,52 @@
+package model;
+
+public class Troutemodl {
+	int rid;
+	String sstation;
+	String estation;
+	String depTime;
+	String arrTime;
+	float tprice;
+	
+	public int getRid() {
+		return rid;
+	}
+	public void setRid(int rid) {
+		this.rid = rid;
+	}
+	public String getSstation() {
+		return sstation;
+	}
+	public void setSstation(String sstation) {
+		this.sstation = sstation;
+	}
+	public String getEstation() {
+		return estation;
+	}
+	public void setEstation(String estation) {
+		this.estation = estation;
+	}
+	public String getDepTime() {
+		return depTime;
+	}
+	public void setDepTime(String depTime) {
+		this.depTime = depTime;
+	}
+	public String getArrTime() {
+		return arrTime;
+	}
+	public void setArrTime(String arrTime) {
+		this.arrTime = arrTime;
+	}
+	public float getTprice() {
+		return tprice;
+	}
+	public void setTprice(float tprice) {
+		this.tprice = tprice;
+	}
+	
+	
+
+	
+
+}
